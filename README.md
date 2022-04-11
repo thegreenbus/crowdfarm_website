a7b744df85843e03d52765a5fb4d05cc11d10ca9
georgethedeveloper <georgekarani77@gmail.com>
2022-04-11 05:09:45 +0300

    added postgres database configuration

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Before installing CrowdFarm you will need to make sure you have the minimum serv
 
 ``
 base64:Unt9XP90fzEdX6TwqifA9ehH9eBc0aNe5XuIZqvDESg=
+
+postgres://lioybfrwdcqjue:772989e87182c5045bf9e3d7915da18be0d244226f80b8c09ca34f7070d79568@ec2-23-20-224-166.compute-1.amazonaws
+.com:5432/delgurpigh2skb
 ``
 ### Clone the Repo
 You can clone the repo onto your local machine with the following command:
--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://lioybfrwdcqjue:772989e87182c5045bf9e3d7915da18be0d244226f80b8c09ca34f7070d79568@ec2-23-20-224-166.compute-1.amazonaws
+.com:5432/delgurpigh2skb');
+
 return [
 
     /*
@@ -15,7 +18,10 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'), /*mysql*/
+
+
+
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +71,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
